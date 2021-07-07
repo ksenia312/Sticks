12c538bae3bcc04875657edaf8cb94517bfed86a
ksenia312 <ksynikitina.02@mail.ru>
2021-07-07 19:41:54 +0300

    delete

--- files/index.py
+++ files/index.py
@@ -1,27 +1,6 @@
 from pygame.locals import *
 from func import *
 
-'''
-from tkinter import *
-
-
-def clicked():
-    res = "Привет {}".format(txt.get())
-    lbl.configure(text=res)
-
-
-window = Tk()
-window.title("Добро пожаловать в приложение PythonRu")
-window.geometry('400x250')
-lbl = Label(window, text="Привет")
-lbl.grid(column=0, row=0)
-txt = Entry(window, width=10)
-txt.grid(column=1, row=0)
-btn = Button(window, text="Клик!", command=clicked)
-btn.grid(column=2, row=0)
-window.mainloop()
-
-'''
 running = True
 while running:
     for event in pygame.event.get():
